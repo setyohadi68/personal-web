a4b19b18f390b0283f6e2fd08e42dedf1d973dd8
setyohadi68 <setyohp68@gmail.com>
2024-03-05 15:14:05 +0700

    :portfolio

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="bg-lime-500 
+    md:bg-blue-500 p-6 flex 
+    justify-center 
+    items-center 
+    h-screen"
+    > 
+      <div className="bg-red-100 w-100 w-40 h-30 rounded-[50%]">
+    HOME KAPAL LAWD
+      </div>
+  </div>
+  )
+}
+
+export default App
+
